bc67f3cd2c7678c4c877f2f9b7294c364617a27d
whyammehere <zhangian230791@gmail.com>
2024-09-11 00:23:19 -0400

    fixed import errors

--- Main.lean
+++ Main.lean
@@ -1,45 +1,216 @@
-import «Cs2120f24».Lectures.«02_prop_logic».formal.syntax
+import «Cs2120f24».Lectures.«02_prop_logic».formal.properties
 
-/-
-The first thing to do: open Cs2120f24/Demo/Demo.lean,
-read it, and work through the easy exercises. It will
-help to have been there for Kevin's introduction to all
-this. Feel free to give a holler: sullivan@virginia.edu.
--/
-
-
-/-
-Ok, so you've see that Lean is a proof assistant. But
-there's more! It's not just a proof assistant, it's a
-complete very usable functional programming language.
-What it lacks in libraries it makes up, over languages
-such as OCaml and Haskell, by *also* being a world-class
-proof assistant. From now on you can build systems in
-a cool language with both programming and foundational
-verification facilities.
--/
 
 namespace cs2120f24
 
-def main : IO Unit :=
-  IO.println s!"Hi!"
+open PLExpr
 
-/-
-You can run the main routine right here by using eval.
-Hover over the #eval to see the result of its execution.
-The {hello} string is defined in Cs2120f24/Welcome.lean.
+
+
+/-!
+SYNTAX
 -/
 
-#eval main
+-- Variables
+def v₀ : BoolVar := BoolVar.mk 0
+def v₁ : BoolVar := ⟨1⟩   -- Lean for structure constructor (mk)
+def v₂ : BoolVar := ⟨2⟩
+
+/-!
+Variable expressions
+-/
+def P : PLExpr := PLExpr.var_expr v₀
+def Q : PLExpr := { v₁ }  -- our notation for var_expr constructor
+def R : PLExpr := { v₂ }
 
 /-
-You can also compile and run it as an executable.
-See the documentation for the *lake* command of the
-Lean4 system.
+Operator expression: abstract syntax
 -/
 
-def P_and_Q_absract : PLExpr :=
-  PLExpr.bin_op_expr BinOp.and P Q
-def P_and_O_concrete := P ∧ Q
+def P_and_Q_abstract : PLExpr :=
+  (PLExpr.bin_op_expr BinOp.and P Q)
 
-#reduce truthTableOutputVector (P ∧ Q)
+-- Exact same expression using standard notation
+def P_and_Q_concrete := P ∧ Q
+
+-- de-sugars to the underlying astract syntax
+#reduce P_and_Q_concrete
+
+
+/-!
+INTERPRETATIONS
+
+The apparent ordering is off, backwards from
+what we'd expect. For now it doesn't matter.
+We've got it on our list of things to fix.
+This example counts the number of variables in
+(P ∧ Q), it's 2, and a list of all 2^2 = 4
+possible interpretations for that expression
+are returned.
+-/
+#reduce listListStringFromListInterps
+          (listInterpsFromExpr (P ∧ Q))
+          2
+/-!
+It's often helpful to list arguments
+to functions properly indented across
+multiple lines.
+-/
+
+
+
+/-!
+TRUTH TABLES (output vectors). The returned
+list is the semantic meaning of the single
+given expression under each ]interpretation,
+starting with all true and descending to all
+false. (We have a note to clean this up.)
+-/
+
+#eval! (truthTableOutputVector (P))
+#eval! (truthTableOutputVector (P ∧ Q))
+
+
+/-!
+Properties of Propositions (Expressions)!
+-/
+
+#reduce is_sat ⊤
+#reduce is_sat ⊥
+
+#reduce is_sat P
+#reduce is_unsat P
+#reduce is_valid P
+
+#reduce is_sat ¬P
+#reduce is_unsat ¬P
+#reduce is_valid ¬P
+
+#reduce is_sat (P ∨ ¬P)
+#reduce is_unsat (P ∨ ¬P)
+#reduce is_valid (P ∨ ¬P)
+
+#reduce is_sat (P ∧ ¬P)
+#reduce is_unsat (P ∧ ¬P)
+#reduce is_valid (P ∧ ¬P)
+
+#reduce is_sat (P ∧ Q)
+#reduce is_unsat (P ∧ Q)
+#reduce is_valid (P ∧ Q)
+
+#reduce is_sat (P ∨ Q)
+#reduce is_unsat (P ∨ Q)
+#reduce is_valid (P ∨ Q)
+
+
+/-!
+Homework #1
+
+1.Complete the sentence: If a proposition is not valid,
+then there is at least one __________.
+
+2. Read the following propositions in English and render
+them into the formal language of propositional logic. We
+will get you started by defining three new propositional
+variables: isRaining, streetWet, and sprinklerOn. Now for
+each of the following propositions expressed in English,
+express it formally using these PL variable expressions.
+Here then are our three new PL variable expressions. The
+identifer's we're binding to these terms remind us what
+we are going to want these terms to mean "in the world."
+-/
+
+def itsRaining : PLExpr := PLExpr.var_expr v₀
+def sprinklerOn : PLExpr := PLExpr.var_expr v₁
+def streetWet : PLExpr := PLExpr.var_expr v₂
+
+/-
+For each of the following English language expressions write
+the corresponding expression using our concrete propositional
+logic syntax. Give names to these propositions (PLExpr's) in
+the pattern of p0, p1, p2, as seen below.
+-/
+
+/-!
+It's raining and the sprinkler's on.
+-/
+def p0 : PLExpr := itsRaining ∧ sprinklerOn
+
+/-!
+The sprinler's on and it's raining.
+-/
+def p1  : PLExpr := sprinklerOn ∧ itsRaining
+
+/-!
+If it's raining, then if the sprinkler's on, then it's
+raining and the sprinkler is on. Conditional (if this
+then that) expressions in natural language are written
+formally in propositional and predicate logic using the
+implication (implies) operator, imp (⇒ in our notation).
+-/
+def p2  : PLExpr := (itsRaining ∧ sprinklerOn) ⇒ (itsRaining ∧ sprinklerOn)
+
+/-!
+If it's raining and the sprinkler's running, then it's raining.
+-/
+def p3  : PLExpr := (itsRaining ∧ sprinklerOn) ⇒ itsRaining
+
+/-!
+If it's raining ,then it's raining or the sprinkler's running.
+-/
+def p4  : PLExpr := itsRaining ⇒ (itsRaining ∨ sprinklerOn)
+
+/-!
+If the sprinkler's running, it's raining or the sprinkler's running.
+-/
+def p5  : PLExpr := sprinklerOn ⇒ (itsRaining ∨ sprinklerOn)
+
+/-!
+Whenever it's raining the streets are wet.
+-/
+def p6  : PLExpr := itsRaining ⇒ streetWet
+
+/-!
+Whenever the sprinkler's running the streets are wet.
+-/
+def p7  : PLExpr := sprinklerOn ⇒ streetWet
+
+/-!
+If (a) it's raining or the sprinkler's running, then (b) if
+whenever it's raining then the streets are wet, then (c) if
+whenever the sprinkler's running then the streets are wet, then
+_________. What is the conclusion? Write the expression in PL.
+-/
+def p8  : PLExpr := (itsRaining ∨ sprinklerOn) ⇒ (itsRaining ⇒ streetWet) ⇒ (sprinklerOn ⇒ streetWet) ⇒ streetWet
+
+/-!
+If whenever it's raining, the streets are wet, then whenever the
+streets are wet it's raining.
+-/
+def p9  : PLExpr := (itsRaining ⇒ streetWet) ⇒ (streetWet ⇒ itsRaining)
+
+
+/-!
+If whever it's raining then bottom, then it's not raining.
+-/
+def p10  : PLExpr := (itsRaining ⇒ ⊥) ⇒ (¬ itsRaining)
+
+/-!
+If it's raining or the sprinkler's running then if it's
+not raining then the sprinkler's running.
+-/
+def p11 : PLExpr := (itsRaining ∨ sprinklerOn) ⇒ (¬ itsRaining ⇒ sprinkerOn)
+
+/-!
+If whenever it's raining the streets are wet, then whenever the
+streets are not wet, it must not be raining.
+-/
+def p12 : PLExpr := (itsRaining ⇒ streetWet) ⇒ ((¬ streetWet) ⇒ (¬ itsRaining))
+
+/-!
+-/
+def p13 : PLExpr := sorry
+def p14 : PLExpr := sorry
+def p15 : PLExpr := sorry
+
+end cs2120f24
